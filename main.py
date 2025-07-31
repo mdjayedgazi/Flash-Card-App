dc00975d2271873ef8c33681d038827b89c2efa3
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-08-01 03:23:57 +0600

    add flip Mechanism

--- main.py
+++ main.py
@@ -3,27 +3,40 @@ from tkinter import messagebox
 import pandas as pd
 import random
 import time
-
+current_card = {}
 BACKGROUND_COLOR = "#B1DDC6"
 # # --------------------------- READ THE DATA FILE ------------- # #
 data_file = pd.read_csv("data/french_words.csv")
 to_learn = data_file.to_dict(orient="records")
 
 def next_card():
+    global current_card
+    global flip_time
+    root.after_cancel(flip_time)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title,text= "French")
-    canvas.itemconfig(card_word,text= current_card["French"])
+    canvas.itemconfig(card_title,text= "French",fill = "Black")
+    canvas.itemconfig(card_word,text= current_card["French"],fill="Black")
+    canvas.itemconfig(card_background, image=font_img)
+    flip_time = root.after(3000,flip_card)
+    
+# # ----------------------------- FLIP CART ----------------------- # #
+def flip_card():
+    canvas.itemconfig(card_title, text= "English",fill="white")
+    canvas.itemconfig(card_word,text=current_card["English"],fill="white")
+    canvas.itemconfig(card_background, image=back_img)
 # # ------------------------ UI SETUP ----------------------- # #
 root = Tk()
 root.title("Flash Card")
 root.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+flip_time = root.after(3000,flip_card)
 # Load images(use raw strings or forward slashes)
 Right_img = PhotoImage(file="images/right.png")
 Wrong_img = PhotoImage(file="images/wrong.png")
-card_img  = PhotoImage(file="images/card_front.png")
+font_img  = PhotoImage(file="images/card_front.png")
+back_img  = PhotoImage(file="images/card_back.png")
 # Card Canvas 
 canvas = Canvas(width=800,height=526,highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400,263,image = card_img)
+card_background = canvas.create_image(400,263,image = font_img)
 canvas.grid(row=0,column=0,columnspan=2)
 card_title = canvas.create_text(400,148,font=("Ariel",40,"italic"))
 card_word = canvas.create_text(400,290,font=("Ariel",60,"bold"))

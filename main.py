b89593f3080deaebf302f58e1cc0dc7c19a49547
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-07-31 19:06:24 +0600

    create image canvas right or wrong buttons

--- main.py
+++ main.py
@@ -13,25 +13,19 @@ root.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 # Load images(use raw strings or forward slashes)
 Right_img = PhotoImage(file="images/right.png")
 Wrong_img = PhotoImage(file="images/wrong.png")
-card_img = PhotoImage(file="images/card_front.png")
+card_img  = PhotoImage(file="images/card_front.png")
 
 # Card Canvas 
 canvas = Canvas(width=800,height=526,highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.create_image(400,263,image = card_img)
 canvas.grid(row=0,column=0,columnspan=2)
-
-#Labels (centered on card)
-con_label = Label(text="French",font=("Arial",40,"italic"),bg="white")
-con_label.place(x=400,y=100,anchor='center')
-
-word_label = Label(text="trouve",font=("Arial",60,"bold"),bg="white")
-word_label.place(x=400,y=263,anchor="center")
-
+canvas.create_text(400,148,text="Title",font=("Ariel",40,"italic"))
+canvas.create_text(400,290,text="Word",font=("Ariel",60,"bold"))
 # Button 
-wrong_button = Button(image=Wrong_img,highlightthickness=0,borderwidth=0)
+wrong_button = Button(image=Wrong_img,highlightthickness=0,bd=0)
 wrong_button.grid(row=1,column=0,pady=10)
 
-right_button = Button(image=Right_img,highlightthickness=0,borderwidth=0)
+right_button = Button(image=Right_img,highlightthickness=0,bd=0)
 right_button.grid(row=1,column=1,pady=10)
 
 root.mainloop()
